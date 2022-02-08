d3fe645ea411012c550805f2399ecaaa6eadaf2a
zqprime <zqpirme>
2022-02-08 10:45:20 +0800

    hadoop

--- bigdata/spark-core/src/main/java/club/zqprime/FirstSparkApplication.scala
+++ bigdata/spark-core/src/main/java/club/zqprime/FirstSparkApplication.scala
@@ -8,24 +8,28 @@ import java.io.File
 
 case object FirstSparkApplication{
   def main(args: Array[String]): Unit = {
-    val wordCount = new SparkConf().setMaster("spark://zqprime-desktop:7077").setAppName("WordCount")
-//    val wordCount = new SparkConf().setMaster("local").setAppName("WordCount")
-    val context = new SparkContext(wordCount)
-    val lines: RDD[String] = context.textFile("bigdata/spark-core/target/classes/datas/words")
+//    val wordCount = new SparkConf().setMaster("spark://zqprime-desktop:7077").setAppName("WordCount")
+//    val wordCount = new SparkConf().setMaster("spark://162.14.97.85:7077").setAppName("WordCount")
+    val wordCount = new SparkConf().setMaster("local").setAppName("WordCount")
+    val spark = new SparkContext(wordCount)
+
+    val lines: RDD[String] = spark.textFile("/home/zqprime/IdeaProjects/zqprime-cloud/bigdata/spark-core/src/main/resources/datas/words")
 
     val words: RDD[String] = lines.flatMap(_.split(" "))
     val wordGroup: RDD[(String,Iterable[String])] = words.groupBy(words => words)
 
-    val result:RDD[(String,Int)] = wordGroup.map {
+    val result = wordGroup.map {
       case (word, list) => {
         (word, list.size)
       }
     }
 
+    val array: Array[(String,Int)] = result.collect()
+
     print("===========start============")
-    result.foreach(print)
+    array.foreach(print)
     print("============end==========")
 
-    context.stop()
+    spark.stop()
   }
 }
