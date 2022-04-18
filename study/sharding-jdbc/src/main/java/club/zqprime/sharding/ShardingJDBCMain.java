6b8f4e80130e92cd462fb7be095773b82e77bb85
张祁 <zqprime@163.com>
2022-04-18 14:28:39 +0800

    sharding jdbc 尝试

---
+++ study/sharding-jdbc/src/main/java/club/zqprime/sharding/ShardingJDBCMain.java
@@ -0,0 +1,11 @@
+package club.zqprime.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJDBCMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJDBCMain.class,args);
+    }
+}
