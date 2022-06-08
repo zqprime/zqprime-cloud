9afd3efca28390c76921b26e9b452627f8d86c3b
zqprime <zqpirme>
2022-06-09 00:01:51 +0800

    add sentineldashboard

--- study/study_redis/src/test/java/club/zqprime/test/TestRedis.java
+++ study/study_redis/src/test/java/club/zqprime/test/TestRedis.java
@@ -40,39 +40,52 @@ public class TestRedis {
 
     @Test
     public void testSetString(){
-        redisTemplate.opsForValue().set("studyString",studies);
+        redisTemplate.opsForValue().set("jc#studyString#12323#haha",studies);
+        redisTemplate.opsForValue().set("jc#studyString#12365#lolo",studies);
+    }
+
+    @Test
+    public void testDeleteKey(){
+        Set<String> keys = redisTemplate.keys("jc#studyString#*");
+        System.out.println(keys);
+        assert keys != null;
+        keys.forEach(redisTemplate::delete);
     }
 
     @Test
     public void testSetList(){
         Long list = redisTemplate.opsForList().leftPushAll("valueList", studies);
-        Long list2 = redisTemplate.opsForList().leftPushAll("valueList", books);
+
 
         System.out.println(list);
-        System.out.println(list2);
+//        System.out.println(list2);
     }
 
    @Test
     public void testGetList(){
-        Object list = redisTemplate.opsForList().leftPop("studyList");
-       Object index = redisTemplate.opsForList().index("studyList", 0);
-       System.out.println(index);
-    }
 
-    @Test
-    public void testSetSet(){
-        Long set1 = redisTemplate.opsForSet().add("studySet", studies.get(0));
-        Long set2 = redisTemplate.opsForSet().add("studySet", studies.get(1));
-        Long set3 = redisTemplate.opsForSet().add("studySet", studies.get(2));
-        Long set4 = redisTemplate.opsForSet().add("studySet", studies.get(3));
-        Long setAll = redisTemplate.opsForSet().add("studySet", studies);
-        System.out.println(set1);
     }
+//
+//    @Test
+//    public void testSetSet(){
+//        Long set1 = redisTemplate.opsForSet().add("studySet", studies.get(0));
+//        Long set2 = redisTemplate.opsForSet().add("studySet", studies.get(1));
+//        Long set3 = redisTemplate.opsForSet().add("studySet", studies.get(2));
+//        Long set4 = redisTemplate.opsForSet().add("studySet", studies.get(3));
+//        Long setAll = redisTemplate.opsForSet().add("studySet", studies);
+//        System.out.println(set1);
+//    }
 
     @Test
     public void testGetSet(){
         Boolean isMember = redisTemplate.opsForSet().isMember("studySet", studies);
         System.out.println(isMember);
+    }
+
+    @Test
+    public void testDelString(){
+        Boolean set = redisTemplate.delete("studySet");
+        System.out.println(set);
     }
 
     @Test
