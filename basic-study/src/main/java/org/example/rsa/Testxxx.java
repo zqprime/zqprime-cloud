0907f8d1963b4b16e85cd3be2f70c4bb3f3ee2d3
zqprime <zqprime@163.com>
2022-10-11 20:09:58 +0800

    不知道

---
+++ basic-study/src/main/java/org/example/rsa/Testxxx.java
@@ -0,0 +1,28 @@
+package org.example.rsa;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class Testxxx {
+    public static void main(String[] args) {
+        System.out.println(yuanToFen(new BigDecimal("0.01").toString()));
+    }
+
+    private static String yuanToFen(String amount){
+        NumberFormat format = NumberFormat.getInstance();
+        try{
+            Number number = format.parse(amount);
+            double temp = number.doubleValue() * 100.0;
+            format.setGroupingUsed(false);
+            // 设置返回数的小数部分所允许的最大位数
+            format.setMaximumFractionDigits(0);
+            amount = format.format(temp);
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        return amount;
+    }
+
+}
