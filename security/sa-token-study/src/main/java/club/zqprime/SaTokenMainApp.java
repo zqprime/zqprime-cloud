c185bba2b51c7a136144e8446b43246e8a0ea563
zqprime <zqprime@163.com>
2023-03-12 21:03:26 +0800

    sa-token

---
+++ security/sa-token-study/src/main/java/club/zqprime/SaTokenMainApp.java
@@ -0,0 +1,19 @@
+package club.zqprime;
+
+import cn.dev33.satoken.SaManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SaTokenMainApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SaTokenMainApp.class,args);
+        System.out.println("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
+    }
+}
