2f16340025a865a8371f90eac8cee4b5125800d6
zqprime <zhangqi@zhongkeliantong.com>
2023-03-20 11:08:03 +0800

    sa-token 集成 oauth2.0 进行 授权码模式访问

--- security/sa-token-study/src/main/java/club/zqprime/config/SaOAuth2ServerController.java
+++ security/sa-token-study/src/main/java/club/zqprime/config/SaOAuth2ServerController.java
@@ -3,12 +3,17 @@ package club.zqprime.config;
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.oauth2.config.SaOAuth2Config;
 import cn.dev33.satoken.oauth2.logic.SaOAuth2Handle;
+import cn.dev33.satoken.oauth2.logic.SaOAuth2Util;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Sa-OAuth2 Server端 控制器
  */
@@ -56,6 +61,29 @@ public class SaOAuth2ServerController {
     public SaResult handlerException(Exception e) {
         e.printStackTrace();
         return SaResult.error(e.getMessage());
+    }
+
+    // ---------- 开放相关资源接口： Client端根据 Access-Token ，置换相关资源 ------------
+
+    // 获取Userinfo信息：昵称、头像、性别等等
+    @RequestMapping("/oauth2/userinfo")
+    public SaResult userinfo() {
+        // 获取 Access-Token 对应的账号id
+        String accessToken = SaHolder.getRequest().getParamNotNull("access_token");
+        Object loginId = SaOAuth2Util.getLoginIdByAccessToken(accessToken);
+        System.out.println("-------- 此Access-Token对应的账号id: " + loginId);
+
+        // 校验 Access-Token 是否具有权限: userinfo
+        SaOAuth2Util.checkScope(accessToken, "userinfo");
+
+        // 模拟账号信息 （真实环境需要查询数据库获取信息）
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("nickname", "shengzhang_");
+        map.put("avatar", "http://xxx.com/1.jpg");
+        map.put("age", "18");
+        map.put("sex", "男");
+        map.put("address", "山东省 青岛市 城阳区");
+        return SaResult.data(map);
     }
 
 }
